9b8d55d8411bcb41dc9fc599cbfb941120710b6c
jp <johnamata@protonmail.com>
2022-06-28 15:05:23 +0800

    add bytecode class for machine implementation

---
+++ src/com/johnamata/parrvm/Bytecode.java
@@ -0,0 +1,9 @@
+package com.johnamata.parrvm;
+
+/**
+ * define bytecode for machine to implement it
+ */
+public class Bytecode {
+    public static final int IADD = 1;
+    //TODO: add the rest of instructions
+}
