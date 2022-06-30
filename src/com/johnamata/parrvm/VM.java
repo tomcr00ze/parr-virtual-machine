fbc6470bcfc51d50a8d3194011188a0a1ee03058
jp <johnamata@protonmail.com>
2022-06-30 23:47:19 +0800

    vm, datasize, comments

--- src/com/johnamata/parrvm/VM.java
+++ src/com/johnamata/parrvm/VM.java
@@ -10,8 +10,10 @@ public class VM {
     int sp; //stack pointer
     int fp; //frame
 
-    public VM(int[] code, int main) {
+    public VM(int[] code, int main, int datasize) {
+        //when executing, we create VM that has code, and it starts with main, also memory to use
         this.code = code;
         this.ip = main;
+        data = new int[datasize];
     }
 }
