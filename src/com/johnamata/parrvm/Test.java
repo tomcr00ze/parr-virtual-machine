68ddb634d14579bf721f6b6fc3960f01bdd7da5b
jp <johnamata@protonmail.com>
2022-06-30 23:40:07 +0800

    test

--- src/com/johnamata/parrvm/Test.java
+++ src/com/johnamata/parrvm/Test.java
@@ -1,4 +1,10 @@
 package com.johnamata.parrvm;
-
+import static com.johnamata.parrvm.Bytecode.*;
 public class Test {
+    static int[] hello = { //for testing
+            HALT
+    };
+    public static void main(String[] args) {
+
+    }
 }
